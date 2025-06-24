d4d49f456a5b9f0eb8cfcc062749b26deb59ab06
Suzuki-KA <23fi055@ms.dendai.ac.jp>
2025-06-24 22:25:41 +0900

    decorate

--- app.js
+++ app.js
@@ -9,16 +9,26 @@ let connects = [];
 
 app.use(express.static("public"));
 
+function decorateMessage(message) {
+  const index = 1;
+  const content = " ".repeat(index) + message + " ".repeat(index);
+  const border = "+".padEnd(content.length + 1, "-") + "+";
+  const middle = "|" + content + "|";
+  return `${border}\n${middle}\n${border}`;
+}
+
 app.ws("/ws", (ws, req) => {
   connects.push(ws);
 
   ws.on("message", (message) => {
     console.log("Received:", message);
 
+    const decorated = decorateMessage(message.toString());
+
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
         // Check if the connection is open
-        socket.send(message);
+        socket.send(decorated);
       }
     });
   });
